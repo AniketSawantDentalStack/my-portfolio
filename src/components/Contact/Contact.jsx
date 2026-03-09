acf0cfe9a21730c60f76539bb37cef54d624d832
Aniket Sawant <141298251+AniketSawantDentalStack@users.noreply.github.com>
2026-03-09 21:40:53 +0530

    Update EmailJS service and template IDs

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -12,10 +12,10 @@ const Contact = () => {
 
     emailjs
       .sendForm(
-        "service_axbtt7a",  // Replace with your EmailJS Service ID
-        "template_1ziboq3",  // Replace with your EmailJS Template ID
+        "service_5e01qmh",  // Replace with your EmailJS Service ID
+        "template_x8enpdj",  // Replace with your EmailJS Template ID
         form.current,
-        "Rz7W9pVF0HdDryNNL"  // Replace with your EmailJS Public Key
+        "pdGIZbnHW_aRGEJVB"  // Replace with your EmailJS Public Key
       )
       .then(
         () => {
